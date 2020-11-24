5f953d3cfd48c8207f74a0ef5ae8bd6898029564
Baskoekkoek1 <baskoekkoek@gmail.com>
2020-11-24 12:17:25 +0100

    set up user-gallery relation

--- models/gallery.js
+++ models/gallery.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class gallery extends Model {
     /**
@@ -10,15 +8,18 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      gallery.belongsTo(models.user);
     }
-  };
-  gallery.init({
-    title: DataTypes.STRING,
-    description: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'gallery',
-  });
+  }
+  gallery.init(
+    {
+      title: DataTypes.STRING,
+      description: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "gallery",
+    }
+  );
   return gallery;
 };
