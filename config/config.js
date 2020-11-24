b269ca361488c5103101210f145569dc4e92e74c
Baskoekkoek1 <baskoekkoek@gmail.com>
2020-11-24 12:23:25 +0100

    hide elephantSQL

---
+++ config/config.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    url:
+      "postgres://dlsrtlfb:J2VJt8iqDShnVpkc_TVlKDMC4DWdhm2E@hattie.db.elephantsql.com:5432/dlsrtlfb",
+    dialect: "postgres",
+    operatorsAliases: "0",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
