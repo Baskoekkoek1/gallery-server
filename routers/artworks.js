b1aa7c056b9f36c62a820344b4d0d6d373e1fda8
Baskoekkoek1 <baskoekkoek@gmail.com>
2020-12-16 17:16:09 +0100

    add clg

--- routers/artworks.js
+++ routers/artworks.js
@@ -7,15 +7,20 @@ const router = new Router();
 router.get("/", async (req, res) => {
   try {
     const { apiArtworksUrl } = req.query;
+    console.log("Artworks URL", apiArtworksUrl);
     const decodedApiArtworksUrl = decodeURIComponent(apiArtworksUrl);
+    console.log("Decoded artworks URL", apiArtworksUrl);
     if (!apiArtworksUrl) {
       return res.status(400).send({ message: "Please provide link" });
     }
+    console.log("API token", apiToken);
     const response = await axios.get(decodedApiArtworksUrl, {
       headers: { "X-XAPP-Token": apiToken },
     });
+    console.log("Response", response);
     return res.status(200).send(response.data);
   } catch (error) {
+    console.log("Error", error);
     return res.status(400).send({ message: "Something went wrong, sorry" });
   }
 });
